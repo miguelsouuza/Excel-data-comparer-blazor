1a29493e1e9cf73c6fdf4ea03a4619025036f225
Miguel de Souza Silva <miguel.silva@mstrategy.com.br>
2026-05-12 14:24:03 -0300

    feat(cache): Adicionando implantação de cache

--- Models/AppState.cs
+++ Models/AppState.cs
@@ -7,9 +7,12 @@
     public List<string> IdsB { get; set; } = new List<string>();
     public List<string> ColunasComparar { get; set; } = new List<string>();
     public Dictionary<string, string> Mapeamento { get; set; } = new Dictionary<string, string>();    
+    public Dictionary<string, string> MapeamentoUI { get; set; } = new Dictionary<string, string>();    
     public MemoryStream StreamA { get; set; }= new MemoryStream();
     public MemoryStream StreamB { get; set; }= new MemoryStream();
     public string NomeArquivoA { get; set; }= string.Empty;
     public string NomeArquivoB { get; set; }= string.Empty;
     public string DelimitadorCsv { get; set; } = ";";
+    public bool ShowMappingHeader { get; set; } = false;
+    public bool ShowMappingColumn { get; set; } = false;
 }
